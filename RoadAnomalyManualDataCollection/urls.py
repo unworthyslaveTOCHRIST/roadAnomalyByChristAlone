9ccca8ed33b1ae3e22c932f8110c587227668b21
unworthyslaveTOCHRIST <righteousnessbyfaithinjesus@gmail.com>
2025-09-30 14:11:07 +0100

    Gracious final backend logic change

--- RoadAnomalyManualDataCollection/urls.py
+++ RoadAnomalyManualDataCollection/urls.py
@@ -64,7 +64,7 @@ class RoadAnomalyManualDataCollectionViewSet(viewsets.ModelViewSet):
             except Exception as e:
                 print(f"⚠️ Error in line {i + 1}: {line}\n  ↳ Exception: {e}")
                 continue
-        return Response(f" Supposed to receive {line_count} lines, each with {len(parts)} fields", status=status.HTTP_201_CREATED)
+        return Response(f" Supposed to receive {line_count} lines, each with {len(parts)} columns", status=status.HTTP_201_CREATED)
         #return Response(f"Received {len(saved_items)} more rows, Current Labelled Data Size : {RoadAnomalyManualDataCollection.objects.all().count()} rows", status=status.HTTP_201_CREATED)
 
     
